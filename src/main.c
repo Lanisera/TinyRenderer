803f6f2644840543ed660e397797f7ab47836a55
laniser <laniser0905@gmail.com>
2025-04-21 11:57:08 +0800

    添加并解析obj文件格式

--- src/main.c
+++ src/main.c
@@ -2,16 +2,17 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "display.h"
 #include "triangle.h"
 #include "vector.h"
 #include "mesh.h"
+#include "array.h"
 
 vec3_t camera_position = { .x = 0, .y = 0, .z = -5};
-vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0};
 
-triangle_t triangle_project[N_MESH_FACES];
+triangle_t* triangle_to_render = NULL;
 
 const int fov_factor = 512;
 
@@ -31,6 +32,7 @@ void setup(void)
 			window_height
 	);
 
+	load_obj_mesh_data("./assets/f22.obj");
 }
 
 void process_input(void)
@@ -69,6 +71,9 @@ vec2_t project(vec3_t point)
 
 void update(void)
 {
+	/////////////////////////////////////////////////////////////////////////////////////////
+	/// 稳定渲染帧率
+	/////////////////////////////////////////////////////////////////////////////////////////
 	int wait_time = FRAME_TIME_TARGET - (SDL_GetTicks() - previous_time);
 
 	if (wait_time > 0 && wait_time < FRAME_TIME_TARGET) {
@@ -76,26 +81,29 @@ void update(void)
 	}
 
 	previous_time = SDL_GetTicks();
-
-
-	cube_rotation.y += 0.005;
-	cube_rotation.x += 0.005;
-	cube_rotation.z += 0.005;
+	/////////////////////////////////////////////////////////////////////////////////////////
 	
-	for (int i = 0; i < N_MESH_FACES; i ++) {
+	triangle_to_render = NULL;
+
+	mesh.rotation.x += 0.005;
+	mesh.rotation.y += 0.000;
+	mesh.rotation.z += 0.000;
+	
+	int num_face = array_length(mesh.faces);
+	for (int i = 0; i < num_face; i ++) {
 		vec3_t triangle_point[3];
-		triangle_point[0] = mesh_vertices[mesh_faces[i].a];
-		triangle_point[1] = mesh_vertices[mesh_faces[i].b];
-		triangle_point[2] = mesh_vertices[mesh_faces[i].c];
+		triangle_point[0] = mesh.vertices[mesh.faces[i].a - 1];
+		triangle_point[1] = mesh.vertices[mesh.faces[i].b - 1];
+		triangle_point[2] = mesh.vertices[mesh.faces[i].c - 1];
 
 		triangle_t triangle;
 
 		for (int j = 0; j < 3; j ++) {
 			vec3_t translate_point = triangle_point[j];
 			
-			translate_point = vec3_rotate_x(translate_point, cube_rotation.x);
-			translate_point = vec3_rotate_y(translate_point, cube_rotation.y);
-			translate_point = vec3_rotate_z(translate_point, cube_rotation.z);
+			translate_point = vec3_rotate_x(translate_point, mesh.rotation.x);
+			translate_point = vec3_rotate_y(translate_point, mesh.rotation.y);
+			translate_point = vec3_rotate_z(translate_point, mesh.rotation.z);
 			
 			// 计算摄像机距离
 			translate_point.z -= camera_position.z;
@@ -109,15 +117,16 @@ void update(void)
 			triangle.points[j] = projected_point;
 		}
 
-		triangle_project[i] = triangle;
+		array_push(triangle_to_render, triangle);
 	}
 }
 
 void render(void) {
 	draw_grid(0xFFFFFFFF);
 	
-	for (int i = 0; i < N_MESH_FACES; i ++) {
-		triangle_t triangle = triangle_project[i];
+	int num_face = array_length(triangle_to_render);
+	for (int i = 0; i < num_face; i ++) {
+		triangle_t triangle = triangle_to_render[i];
 		draw_triangle(triangle.points[0].x, triangle.points[0].y,
 									triangle.points[1].x, triangle.points[1].y,
 									triangle.points[2].x, triangle.points[2].y,
@@ -127,7 +136,15 @@ void render(void) {
 	render_color_buffer();
 	clear_color_buffer(0x00000000);
 
+	array_free(triangle_to_render);
+
 	SDL_RenderPresent(renderer);
+}
+
+void free_resources(void) {
+	free(color_buffer);
+	array_free(mesh.vertices);
+	array_free(mesh.faces);
 }
 
 int main(void)
@@ -143,6 +160,7 @@ int main(void)
 	}
 
 	destroy_window();
+	free_resources();
 
 	return 0;
 }
--- src/mesh.c
+++ src/mesh.c
@@ -1,8 +1,17 @@
+#include <stdio.h>
+#include <string.h>
 #include "mesh.h"
+#include "array.h"
 #include "triangle.h"
 #include "vector.h"
 
-vec3_t mesh_vertices[N_MESH_VERTICES] = {
+mesh_t mesh = {
+	.vertices = NULL,
+	.faces = NULL,
+	.rotation = {0, 0, 0}
+};
+
+vec3_t cube_vertices[N_CUBE_VERTICES] = {
 	{.x = -1, .y = -1, .z = -1},
 	{.x = -1, .y =  1, .z = -1},
 	{.x =  1, .y =  1, .z = -1},
@@ -14,7 +23,7 @@ vec3_t mesh_vertices[N_MESH_VERTICES] = {
 };
 
 
-face_t mesh_faces[N_MESH_FACES] = {
+face_t cube_faces[N_CUBE_FACES] = {
 	// front
 	{0, 1, 2},
 	{0, 2, 3},
@@ -35,3 +44,49 @@ face_t mesh_faces[N_MESH_FACES] = {
 	{3, 4, 5}
 
 };
+
+void load_cube_mesh_data(void) {
+	for (int i = 0; i < N_CUBE_VERTICES; i++) {
+		array_push(mesh.vertices, cube_vertices[i]);
+	}
+
+	for (int i = 0; i < N_CUBE_FACES; i++) {
+		array_push(mesh.faces, cube_faces[i]);
+	}
+}
+
+void load_obj_mesh_data(char* file) {
+	FILE* fp = fopen(file, "r");
+
+	char line[1024];
+
+	while (fgets(line, sizeof(line), fp)) {
+		if (strncmp(line, "v ", 2) == 0) {
+			vec3_t vertex;
+			sscanf(line, "v %f %f %f", &vertex.x, &vertex.y, &vertex.z);
+			array_push(mesh.vertices, vertex);
+			// printf("%f %f %f\n", vertex.x, vertex.y, vertex.z);
+		}
+		else if (strncmp(line, "f ", 2) == 0) {
+			int vertex_index[3];
+			int texture_index[3];
+			int normal_index[3];
+			sscanf(line,
+						"f %d/%d/%d %d/%d/%d %d/%d/%d", 
+						&vertex_index[0], &texture_index[0], &normal_index[0],
+						&vertex_index[1], &texture_index[1], &normal_index[1],
+						&vertex_index[2], &texture_index[2], &normal_index[2]
+			);
+			
+			face_t face = {
+				.a = vertex_index[0],
+				.b = vertex_index[1],
+				.c = vertex_index[2]
+			};
+			array_push(mesh.faces, face);
+			// printf("%d %d %d\n", face.a, face.b, face.c);
+		}
+	}
+}
+
+
